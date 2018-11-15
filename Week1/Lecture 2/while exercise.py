09c290ef6e050aabd72ecf09e2f89b361081b586
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-15 11:50:12 +0800

    Add files via upload

---
+++ Week1/Lecture 2/while exercise.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Exercise: while exercise 1
+
+#In this problem you'll be given a chance to practice writing some while loops.
+#1. Convert the following into code that uses a while loop.
+#print 2
+#prints 4
+#prints 6
+#prints 8
+#prints 10
+#prints Goodbye!
+
+num = 2
+while num <= 10:
+   print (num)
+   num += 2
+print("Goodbye!")
