8109c1648de09a81406dd223fee78e82ed47edf2
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-17 14:06:36 +0800

    Create Exercise: power recur.py

---
+++ Exercises/Exercise: power recur.py
@@ -0,0 +1,3 @@
+Write a function recurPower(base, exp) which computes  by recursively calling itself to solve a smaller version of the same problem, and then multiplying the result by base to solve the initial problem.
+
+This function should take in two values - base can be a float or an integer; exp will be an integer . It should return one numerical value. Your code must be recursive - use of the ** operator or looping constructs is not allowed.
