ff5ac18236ad2abd23f2058ef8f2ae7727ba3b36
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-17 14:05:51 +0800

    Update Exercise: power iter.py

--- Exercises/Exercise: power iter.py
+++ Exercises/Exercise: power iter.py
@@ -3,3 +3,15 @@
 # This function should take in two values - base can be a float or an integer; exp will be an integer  0. It should return one numerical value. Your code must be iterative - use of the ** operator is not allowed.
 
 def iterPower(base,exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    result = 1
+    while exp > 0:
+        result *= base
+        exp -= 1
+    return result
+
