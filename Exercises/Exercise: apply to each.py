baf02e74e3f6386b6bfc0de6292611d919ed921b
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-19 18:11:57 +0800

    Create Exercise: apply to each.py

---
+++ Exercises/Exercise: apply to each.py
@@ -0,0 +1,18 @@
+# Exercise: apply to each 1
+# testList = [1, -4, 8, -9]
+# >>> print(testList)
+#   [1, 4, 8, 9]
+
+applyToEach(testList,abs)
+
+
+# Exercise: apply to each 2
+#  >>> print(testList)
+#   [2, -3, 9, -8]
+
+def addOne(a)
+    return a+1
+
+applyToEach(testList,addOne)
+
+
