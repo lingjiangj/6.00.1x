fc0a611626487c627eacd83caefe915ea3a0c8bc
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-19 18:07:05 +0800

    Update Exercise: odd tuples.py

--- Exercises/Exercise: odd tuples.py
+++ Exercises/Exercise: odd tuples.py
@@ -1,1 +1,18 @@
+# Write a procedure called oddTuples, which takes a tuple as input, and returns a new tuple as output, 
+# where every other element of the input tuple is copied, starting with the first one. 
+# So if test is the tuple ('I', 'am', 'a', 'test', 'tuple'), then evaluating oddTuples on this input would return the tuple ('I', 'a', 'tuple').
 
+
+def oddTuples(aTup):
+    '''
+    aTup: a tuple
+    
+    returns: tuple, every other element of aTup. 
+    '''
+    index = 0
+    result = ()
+    while index <= len(aTup):
+        result += (aTup[index],)
+        indext +=2
+        
+    return result
