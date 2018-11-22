2eab9e4718e3498d3f766a96faebcd8b02f483f1
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-22 15:55:06 +0800

    Create Exercise: hand.py

---
+++ Exercises/Exercise: hand.py
@@ -0,0 +1,14 @@
+def update(self, word):
+        """
+        Does not assume that self.hand has all the letters in word.
+
+        Updates the hand: if self.hand does have all the letters to make
+        the word, modifies self.hand by using up the letters in the given word.
+
+        Returns True if the word was able to be made with the letter in
+        the hand; False otherwise.
+        
+        word: string
+        returns: Boolean (if the word was or was not made)
+        """
+        # Make a copy of the hand, and try to update it
