de8606c177a97ec0ce72460b6be0552c5a63b3b6
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-23 14:42:53 +0800

    Update Problem 7.py

--- Final Exam/Problem 7.py
+++ Final Exam/Problem 7.py
@@ -1,9 +1,11 @@
+# problem 7
 class Container(object):
     """ Holds hashable objects. Objects may occur 0 or more times """
     def __init__(self):
         """ Creates a new container with no objects in it. I.e., any object 
             occurs 0 times in self. """
         self.vals = {}
+    
     def insert(self, e):
         """ assumes e is hashable
             Increases the number times e occurs in self by 1. """
@@ -11,6 +13,7 @@ class Container(object):
             self.vals[e] += 1
         except:
             self.vals[e] = 1
+    
     def __str__(self):
         s = ""
         for i in sorted(self.vals.keys()):
@@ -18,14 +21,22 @@ class Container(object):
                 s += str(i)+":"+str(self.vals[i])+"\n"
         return s
   
- class Bag(Container):
+class Bag(Container):
     def remove(self, e):
         """ assumes e is hashable
             If e occurs in self, reduces the number of 
             times it occurs in self by 1. Otherwise does nothing. """
-        # write code here
-
+        if e in self.vals:
+            self.vals[e] -= 1
+        
     def count(self, e):
         """ assumes e is hashable
             Returns the number of times e occurs in self. """
-        # write code here
+        if e in self.vals:
+            return self.vals[e]
+        else:
+            return 0
+        
+
+        
+        
